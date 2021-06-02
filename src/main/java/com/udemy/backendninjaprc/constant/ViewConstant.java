c3487ff7415708b3540af356c66d0fc4478d9239
Edson Valenzuela <edsonvalher@gmail.com>
2021-06-02 12:24:43 -0600

    añadiendo javadocs

--- src/main/java/com/udemy/backendninjaprc/constant/ViewConstant.java
+++ src/main/java/com/udemy/backendninjaprc/constant/ViewConstant.java
@@ -1,7 +1,11 @@
 package com.udemy.backendninjaprc.constant;
 
+/**
+ * Las constantes de las vistas
+ */
 public class ViewConstant {
 
+    /** The request time interceptor */
     public static final String CONTACT_FORM = "contactform";
     public static final String CONTACTS = "contacts";
     public static final String LOGIN = "login";
