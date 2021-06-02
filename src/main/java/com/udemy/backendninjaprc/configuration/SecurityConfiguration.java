485f0d3530563d3d8bea990df869bfb807a87034
Edson Valenzuela <edsonvalher@gmail.com>
2021-06-02 11:03:38 -0600

    configuración adaptandola al proyecto con las urls y el la entidad usuario

---
+++ src/main/java/com/udemy/backendninjaprc/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.udemy.backendninjaprc.configuration;
+
+import com.udemy.backendninjaprc.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("userService")
+    private UserService userService;
+
+    @Autowired
+    public void ConfigureGloba(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/css/*", "/imgs/*").permitAll().anyRequest().authenticated().and()
+                .formLogin().loginPage("/login").loginProcessingUrl("/logincheck").usernameParameter("username")
+                .passwordParameter("password").defaultSuccessUrl("/loginsuccess").permitAll().and().logout()
+                .logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll();
+    }
+
+}
