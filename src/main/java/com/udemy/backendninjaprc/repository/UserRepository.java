1c38150636d37d0310f99ed1aed22ef45011f1de
Edson Valenzuela <edsonvalher@gmail.com>
2021-06-02 10:52:39 -0600

    implements UserDetailsService

---
+++ src/main/java/com/udemy/backendninjaprc/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.udemy.backendninjaprc.repository;
+
+import java.io.Serializable;
+
+import com.udemy.backendninjaprc.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Serializable> {
+
+    public abstract User findByUsername(String username);
+
+}
